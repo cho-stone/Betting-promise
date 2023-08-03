7160a1f5fd5ab6f322b6e2fe0b104a8b4bbc40cf
cho-stone <cba64511@gmail.com>
2023-08-03 17:22:28 +0900

    add map function

--- app/src/main/java/com/PACOsoft/promise_betting/Map.java
+++ app/src/main/java/com/PACOsoft/promise_betting/Map.java
@@ -2,20 +2,32 @@ package com.PACOsoft.promise_betting;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.naver.maps.geometry.LatLng;
 import com.naver.maps.map.CameraPosition;
+import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapView;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.util.FusedLocationSource;
 
 public class Map extends AppCompatActivity implements OnMapReadyCallback
 {
 
     private MapView mapView;
     private static NaverMap naverMap;
+    private static final int PERMISSION_REQUEST_CODE = 100; // 권한 부여 코드 생성
+    private static final String[] PERMISSIONS = { //위치 권한을 담은 배열
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    };
+    private FusedLocationSource locationSource; //현재 위치를 나타내줄 로케이션 소스
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -26,17 +38,26 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback
         mapView = (MapView) findViewById(R.id.map_view);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
+        locationSource = new FusedLocationSource(this, PERMISSION_REQUEST_CODE);
     }
 
     @Override
     public void onMapReady(@NonNull NaverMap naverMap)
     {
         this.naverMap = naverMap;
-
-        CameraPosition cameraPosition = new CameraPosition(
-                new LatLng(33.38, 126.55), // 위치 지정
-                9 // 줌 레벨
-        );
-        naverMap.setCameraPosition(cameraPosition); //변경된 위치 반영
+        naverMap.setLocationSource(locationSource); //현재 위치 반영
+        ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_REQUEST_CODE); //권한 확인
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // request code와 권한 획득 여부 확인
+        if(requestCode == PERMISSION_REQUEST_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+            }
+        }
+    }
+
 }
