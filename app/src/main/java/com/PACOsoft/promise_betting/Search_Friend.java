8ecd588a8852c66ea434ec0aba1a346c086bfef9
kangho1117 <kangho1117@naver.com>
2023-08-06 01:41:19 +0900

    자기 자신 친구 추가 방지

--- app/src/main/java/com/PACOsoft/promise_betting/Search_Friend.java
+++ app/src/main/java/com/PACOsoft/promise_betting/Search_Friend.java
@@ -67,46 +67,51 @@ public class Search_Friend extends AppCompatActivity {
                     users.add(snapshot.getValue(User.class));
                 }
                 TextView textView = (TextView) findViewById(R.id.et_search);//텍스트뷰 참조 객체 선언
-
-                if (users.stream().parallel().anyMatch(u -> u.getId().equals(textView.getText().toString()))) {//텍스트뷰에서 가져온 텍스트와 동일한 id가 DB에 있는지 확인
-
-                    if (users.stream().parallel().anyMatch(u -> u.getId().equals(myId))) {//myId와 동일한 id가 DB에 있는지 확인 //친구 추가 중복 방지 기능
-                        Optional<User> anyElement = users.stream().parallel().filter(u -> u.getId().equals(myId)).findFirst();
-                        //User에서 id가 myId와 동일한 객체를 필터링
-                        String[] s = anyElement.get().getFriendsId().split(" ");//위에서 필터링한 객체의 FriendsId를 공백을 기준으로 스플릿 해서 배열에 저장
-                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                            User user = snapshot.getValue(User.class); // 만들어뒀던 User 객체에 데이터를 담는다
-                            for (String t : s) {
-                                if (user.getId().equals(t) && user.getId().equals(textView.getText().toString())) {
-                                    Toast toast = Toast.makeText(getApplicationContext(), "이미 친구 목록에 존재하는 친구입니다.", Toast.LENGTH_SHORT);
-                                    toast.show();
-                                    isFriendAlreadyExist = 1;
-                                }
-
-                            }
-                        }
-                        adapter.notifyDataSetChanged();//리스트 저장 및 새로고침
-
-                    }
-
-                    if (isFriendAlreadyExist == 0) {//중복된 친구가 없는 경우에만 추가 가능
-                        anyElement = users.stream().parallel().filter(u -> u.getId().equals(textView.getText().toString())).findFirst();
-                        anyElement2 = users.stream().parallel().filter(u -> u.getId().equals(myId)).findFirst();
-                        //DB에 동일한 ID가 존재한다면 텍스트뷰 참조 객체에서 입력된 텍스트 받아와서 DB의 id와 동일한 객체 찾음
-                        String s = anyElement.get().getId();
-                        String s2 = anyElement2.get().getFriendsId();
-                        temp = s2 + " " + s;
-                        //찾은 객체의 id가져와서 스트링에 저장
-                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                            User user = snapshot.getValue(User.class); // 만들어뒀던 User 객체에 데이터를 담는다
-                            if (user.getId().equals(s))
-                                arrayList.add(user);//담은 데이터를 어레이리스트에 넣고 리사이클러뷰로 보낼 준비함
-                        }
-                        adapter.notifyDataSetChanged();//리스트 저장 및 새로고침
-                    }
-                } else {
-                    Toast toast = Toast.makeText(getApplicationContext(), "일치하는 ID가 없습니다.", Toast.LENGTH_SHORT);
+                if (textView.getText().toString().equals(myId)) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "자기 자신은 추가할 수 없습니다.", Toast.LENGTH_SHORT);
                     toast.show();
+                    isFriendAlreadyExist = 1;
+                } else {
+                    if (users.stream().parallel().anyMatch(u -> u.getId().equals(textView.getText().toString()))) {//텍스트뷰에서 가져온 텍스트와 동일한 id가 DB에 있는지 확인
+
+                        if (users.stream().parallel().anyMatch(u -> u.getId().equals(myId))) {//myId와 동일한 id가 DB에 있는지 확인 //친구 추가 중복 방지 기능
+                            Optional<User> anyElement = users.stream().parallel().filter(u -> u.getId().equals(myId)).findFirst();
+                            //User에서 id가 myId와 동일한 객체를 필터링
+                            String[] s = anyElement.get().getFriendsId().split(" ");//위에서 필터링한 객체의 FriendsId를 공백을 기준으로 스플릿 해서 배열에 저장
+                            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                User user = snapshot.getValue(User.class); // 만들어뒀던 User 객체에 데이터를 담는다
+                                for (String t : s) {
+                                    if (user.getId().equals(t) && user.getId().equals(textView.getText().toString())) {
+                                        Toast toast = Toast.makeText(getApplicationContext(), "이미 친구 목록에 존재하는 친구입니다.", Toast.LENGTH_SHORT);
+                                        toast.show();
+                                        isFriendAlreadyExist = 1;
+                                    }
+
+                                }
+                            }
+                            adapter.notifyDataSetChanged();//리스트 저장 및 새로고침
+
+                        }
+
+                        if (isFriendAlreadyExist == 0) {//중복된 친구가 없는 경우에만 추가 가능
+                            anyElement = users.stream().parallel().filter(u -> u.getId().equals(textView.getText().toString())).findFirst();
+                            anyElement2 = users.stream().parallel().filter(u -> u.getId().equals(myId)).findFirst();
+                            //DB에 동일한 ID가 존재한다면 텍스트뷰 참조 객체에서 입력된 텍스트 받아와서 DB의 id와 동일한 객체 찾음
+                            String s = anyElement.get().getId();
+                            String s2 = anyElement2.get().getFriendsId();
+                            temp = s2 + " " + s;
+                            //찾은 객체의 id가져와서 스트링에 저장
+                            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                User user = snapshot.getValue(User.class); // 만들어뒀던 User 객체에 데이터를 담는다
+                                if (user.getId().equals(s))
+                                    arrayList.add(user);//담은 데이터를 어레이리스트에 넣고 리사이클러뷰로 보낼 준비함
+                            }
+                            adapter.notifyDataSetChanged();//리스트 저장 및 새로고침
+                        }
+                    } else {
+                        Toast toast = Toast.makeText(getApplicationContext(), "일치하는 ID가 없습니다.", Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
                 }
             }
 
