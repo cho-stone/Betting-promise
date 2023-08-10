a366ece701c6bd08eb70057157b668c2b2fa554b
kangho1117 <kangho1117@naver.com>
2023-08-10 21:02:16 +0900

    .

--- app/src/main/java/com/PACOsoft/promise_betting/view/Test_Signin4.java
+++ app/src/main/java/com/PACOsoft/promise_betting/view/Test_Signin4.java
@@ -24,45 +24,36 @@ public class Test_Signin4 extends AppCompatActivity {
         setContentView(R.layout.activity_test4);
         auth = FirebaseAuth.getInstance();
 
-
     }
 
 
-<<<<<<< HEAD
     public void Test4_btn_signup(View view) {
-        ID = (TextInputEditText)findViewById(R.id.test4_et_ID);
-        Password = (TextView)findViewById(R.id.test4_et_PWCheck);
+        ID = (TextInputEditText) findViewById(R.id.test4_et_ID);
+        Password = (TextView) findViewById(R.id.test4_et_PWCheck);
         signup();
     }
 
-    public void Test4_btn_signup_close(View view){
+    public void Test4_btn_signup_close(View view) {
         finish();
     }
 
     public void signup() {
-=======
-    public void btn_signu(View view) {
-        ID = (TextInputEditText)findViewById(R.id.et_i);
-        Password = (TextView)findViewById(R.id.lo_pw_check);
->>>>>>> e67df6cb5b66c85b57c9db4023345c8f9ff78079
+
         try {
             auth.createUserWithEmailAndPassword(ID.getText().toString().trim(), Password.getText().toString().trim())
                     .addOnCompleteListener(task -> {
-                        if(task.isSuccessful()) {
+                        if (task.isSuccessful()) {
                             Toast.makeText(getApplicationContext(), "계정 생성 성공", Toast.LENGTH_LONG).show();
-                            Log.e("T","성공");
+                            Log.e("T", "성공");
                             finish();
-                        }
-                        else{
-                            Log.e("T","이미 존재하는 계정");
+                        } else {
+                            Log.e("T", "이미 존재하는 계정");
                             Toast.makeText(getApplicationContext(), "이미 존재하는 계정", Toast.LENGTH_LONG).show();
                         }
                     });
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
-
 }
