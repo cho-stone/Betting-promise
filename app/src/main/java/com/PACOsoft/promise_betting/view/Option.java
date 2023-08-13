0b44ad4936ecbd65efacdbdd8c859c9e1c73854e
kangho1117 <kangho1117@naver.com>
2023-08-13 10:51:25 +0900

    option 변경

--- app/src/main/java/com/PACOsoft/promise_betting/view/Option.java
+++ app/src/main/java/com/PACOsoft/promise_betting/view/Option.java
@@ -7,36 +7,82 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.PACOsoft.promise_betting.R;
 import com.PACOsoft.promise_betting.view.MainActivity;
+import com.google.firebase.auth.FirebaseAuth;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.PACOsoft.promise_betting.R;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class Option extends AppCompatActivity {
+    private FirebaseAuth mAuth ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_option);
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
-    //로그아웃하면 MainActivity로 이동하는 버튼 구현
-    public void btnLogOutClicked(View view) {
-        SharedPreferences auto = getSharedPreferences("auto", Activity.MODE_PRIVATE);
-        SharedPreferences.Editor editor = auto.edit();
-        editor.clear(); //오토 로그인 값을 싹 비우고 저장
-        editor.commit();
-        //로그아웃 버튼 누르면 기존에 실행되던 엑티비티 모두 종료하고 로그인 화면 엑티비티로 이동
-        Intent i = new Intent(this, MainActivity.class);
-        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(i);
+    private void signOut() {
+        FirebaseAuth.getInstance().signOut();
     }
 
-    public void btnExitClicked(View view) {
-        //앱 종료 버튼 누르면 모든 엑티비티 종류 후 앱 종료
-        finishAffinity();
+    private void revokeAccess() {
+        mAuth.getCurrentUser().delete();
+    }
+    //@Override
+//    public void onClick(View v) {
+//        if(v.getId() == R.id.btn_signout) {
+//            signOut();
+//            //finishAffinity();
+//            Intent i = new Intent(this, Test_Signin3.class);
+//            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//            startActivity(i);
+//        } else if (v.getId() == R.id.btn_revoke) {
+//            revokeAccess();
+//            finishAffinity();
+//        }
+//        else{
+//            finishAffinity();
+//            System.runFinalization();
+//            System.exit(0);
+//        }
+//    }
+
+    public void btnSignOut() {
+        signOut();
+        System.runFinalization();
+        System.exit(0);
+//        finishAffinity();
+//        Intent i = new Intent(this, Test_Signin3.class);
+//        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//        startActivity(i);
+    }
+
+    public void btnExitClicked() {
+        //finishAffinity();
         System.runFinalization();
         System.exit(0);
     }
+
+    public void btn_revoke() {
+        revokeAccess();
+        System.runFinalization();
+        System.exit(0);
+        //finishAffinity();
+    }
+
 
 
 }
