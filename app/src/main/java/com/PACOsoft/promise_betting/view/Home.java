0b151b8567921d889eefc39611fe074d8905b9ac
kangho1117 <kangho1117@naver.com>
2023-08-21 17:20:04 +0900

    항상 보고있는 것 -> 버튼 누르면 갱신으로 변경

--- app/src/main/java/com/PACOsoft/promise_betting/view/Home.java
+++ app/src/main/java/com/PACOsoft/promise_betting/view/Home.java
@@ -104,7 +104,7 @@ public class Home extends AppCompatActivity {
         userArrayList = new ArrayList<>();// User 객체를 담을 ArrayList(Adapter쪽으로 날릴 것임)
         database = FirebaseDatabase.getInstance();//파이어베이스 데이터베이스 연결
         databaseReference = database.getReference("User");//DB테이블 연결, 파이어베이스 콘솔에서 User에 접근
-        databaseReference.addValueEventListener(new ValueEventListener() {
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 //파이어베이스 데이터베이스의 데이터를 받아오는 곳
@@ -158,7 +158,7 @@ public class Home extends AppCompatActivity {
         promiseArrayList = new ArrayList<>();// User 객체를 담을 ArrayList(Adapter쪽으로 날릴 것임)
         database = FirebaseDatabase.getInstance();//파이어베이스 데이터베이스 연결
         databaseReference = database.getReference("User");//DB테이블 연결, 파이어베이스 콘솔에서 User에 접근
-        databaseReference.addValueEventListener(new ValueEventListener() {
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 //파이어베이스 데이터베이스의 데이터를 받아오는 곳
@@ -186,7 +186,7 @@ public class Home extends AppCompatActivity {
         //약속 정보 DB에 접근해서 내 약속과 일치하는 약속들만 어렙터 연결해서 리사이클러뷰에 띄워주고 newPromises에 저장
         newPromises = "";//newPromises 초기화
         databaseReference = database.getReference("Promise");//DB테이블 연결, 파이어베이스 콘솔에서 History에 접근
-        databaseReference.addValueEventListener(new ValueEventListener() {
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 //파이어베이스 데이터베이스의 데이터를 받아오는 곳
