b0c1bd66cbcd9d86990749b83da3d0e60b5cd9ac
kangho1117 <kangho1117@naver.com>
2023-08-24 20:56:04 +0900

    invite_Friend 리스너 수정

--- app/src/main/java/com/PACOsoft/promise_betting/view/Invite_Friend.java
+++ app/src/main/java/com/PACOsoft/promise_betting/view/Invite_Friend.java
@@ -32,14 +32,14 @@ public class Invite_Friend extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
-    private ArrayList<User> arrayList;
     private FirebaseDatabase database;
-    private DatabaseReference databaseReference;
-    private String myId;
-    private String UID;
-    private String TAG;
-    private HashSet<String> hashSet = new HashSet<>();//중복 방지 위해 해쉬셋 이용,ID 저장용
-    private HashSet<String> hashSet2 = new HashSet<>();//중복 방지 위해 해쉬셋 이용,nickname 저장용
+    private DatabaseReference databaseReference, databaseReference2;
+    private String UID, TAG;
+    private String[] friends;
+    private ValueEventListener getInviteFriendListValueEventLister, getInviteFriendListValueEventLister2, getInviteFriendListValueEventLister3, getInviteFriendListValueEventLister4;
+    private HashSet<String> hashSet = new HashSet<>();//중복 방지 위해 해쉬셋 이용, UID 저장용
+    private HashSet<String> hashSet2 = new HashSet<>();//중복 방지 위해 해쉬셋 이용, nickname 저장용
+    private HashSet<String> hashSet3 = new HashSet<>();//중복 방지 위해 해쉬셋 이용, id 저장용
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,110 +48,73 @@ public class Invite_Friend extends AppCompatActivity {
         Intent intent = getIntent();
         TAG = "Invite_Friend";
         UID = intent.getStringExtra("UID"); //Home에서 intent해준 UID를 받아옴
-        recyclerView = findViewById(R.id.inviteFriendsRecyclerview); // 아이디 연결
-        recyclerView.setHasFixedSize(true);//리사이클러뷰 성능 강화
-        layoutManager = new LinearLayoutManager(this);//콘텍스트 자동입력
-        recyclerView.setLayoutManager(layoutManager);
-        arrayList = new ArrayList<>();// User 객체를 담을 ArrayList(Adapter쪽으로 날릴 것임)
-        database = FirebaseDatabase.getInstance();//파이어베이스 데이터베이스 연결
-        databaseReference = database.getReference("User");//DB테이블 연결, 파이어베이스 콘솔에서 User에 접근
-        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                //파이어베이스 데이터베이스의 데이터를 받아오는 곳
-                arrayList.clear(); //기존 배열리스트를 초기화
-
-                ArrayList<User> users = new ArrayList<>();
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    users.add(snapshot.getValue(User.class));
-                }
-
-                if (users.stream().parallel().anyMatch(u ->u.getUID().equals(UID))) {//myId와 동일한 id가 DB에 있는지 확인
-                    Optional<User> anyElement = users.stream().parallel().filter(u -> u.getUID().equals(UID)).findFirst();
-                    String[] s = anyElement.get().getFriendsUID().split(" ");//위에서 필터링한 객체의 FriendsId를 공백을 기준으로 스플릿 해서 배열에 저장
-                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                        User user = snapshot.getValue(User.class); // 만들어뒀던 User 객체에 데이터를 담는다
-                        for (String t : s) {
-                            if (user.getId().equals(t))
-                                arrayList.add(user);//담은 데이터를 어레이리스트에 넣고 리사이클러뷰로 보낼 준비함
-                        }
-                    }
-                    adapter.notifyDataSetChanged();//리스트 저장 및 새로고침
-                }
-            }
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                //DB를 가져오는 중에 에러 발생 시 어떤걸 띄울 것인가
-                Log.e(TAG, String.valueOf(databaseError.toException()));//에러문 출력
-            }
-        });
-
-        adapter = new User_List_Adapter(arrayList, this);
-        recyclerView.setAdapter(adapter); //리사이클러뷰에 어댑터 연결
-
+        view_my_all_friend();
     }
-
 
     public void btn_SearchFriend(View view) {//검색 버튼 누르면 실행
-
+        databaseReference.removeEventListener(getInviteFriendListValueEventLister);
         TextView textView = (TextView) findViewById(R.id.et_search);//텍스트뷰 참조 객체 선언
-        databaseReference.addValueEventListener(new ValueEventListener() {
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                //파이어베이스 데이터베이스의 데이터를 받아오는 곳
-                arrayList.clear(); //기존 배열리스트를 초기화
-                ArrayList<User> users = new ArrayList<>();
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    users.add(snapshot.getValue(User.class));
-                }
-
-                if (users.stream().parallel().anyMatch(u -> u.getUID().equals(UID))) {//myId와 동일한 id가 DB에 있는지 확인
-                    Optional<User> anyElement = users.stream().parallel().filter(u -> u.getUID().equals(UID)).findFirst();
-                    //User에서 id가 myId와 동일한 객체를 필터링
-                    String[] s = anyElement.get().getFriendsUID().split(" ");//위에서 필터링한 객체의 FriendsId를 공백을 기준으로 스플릿 해서 배열에 저장
-                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                        User user = snapshot.getValue(User.class); // 만들어뒀던 User 객체에 데이터를 담는다
-                        for (String t : s) {
-                            if (textView.getText().toString().equals("")) {
-                                if (user.getId().equals(t))
-                                    arrayList.add(user);//담은 데이터를 어레이리스트에 넣고 리사이클러뷰로 보낼 준비함
-                            } else if (user.getId().equals(t) && user.getId().equals(textView.getText().toString()))
-                                arrayList.add(user);//담은 데이터를 어레이리스트에 넣고 리사이클러뷰로 보낼 준비함
-                        }
-                    }
-                    adapter.notifyDataSetChanged();//리스트 저장 및 새로고침
+        ArrayList<User> arrayList = new ArrayList<>();// User 객체를 담을 ArrayList(Adapter쪽으로 날릴 것임)
+        recyclerView.setAdapter(adapter); //리사이클러뷰에 어댑터 연결
+        getInviteFriendListValueEventLister4 = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User friend = snapshot.getValue(User.class);
+                if (friend.getId().equals(textView.getText().toString())) {
+                    arrayList.add(friend);
+                    adapter.notifyDataSetChanged();
                 }
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                //DB를 가져오는 중에 에러 발생 시 어떤걸 띄울 것인가
-                Log.e("MainActivity", String.valueOf(databaseError.toException()));//에러문 출력
+            public void onCancelled(@NonNull DatabaseError error) {
             }
-        });
+        };
+        getInviteFriendListValueEventLister3 = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                arrayList.clear(); //기존 배열리스트를 초기화
+                User me = snapshot.getValue(User.class);
+                friends = me.getFriendsUID().split(" ");//위에서 필터링한 객체의 FriendsId를 공백을 기준으로 스플릿 해서 배열에 저장
+                for (String friend : friends) {
+                    databaseReference2 = database.getReference("User").child(friend);
+                    databaseReference2.addListenerForSingleValueEvent(getInviteFriendListValueEventLister4);
+                    databaseReference2.removeEventListener(getInviteFriendListValueEventLister4);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        };
+        databaseReference = database.getReference("User").child(UID);//DB테이블 연결, 파이어베이스 콘솔에서 User에 접근
+        databaseReference.addValueEventListener(getInviteFriendListValueEventLister3);
         adapter = new User_List_Adapter(arrayList, this);
         recyclerView.setAdapter(adapter); //리사이클러뷰에 어댑터 연결
     }
 
-    public void btn_UserClicked(View v) {
+    public void btn_UserClicked(@NonNull View v) {
         ColorDrawable color = (ColorDrawable) v.getBackground();
         int bgcolor = color.getColor();
-        TextView tv_id = v.findViewById(R.id.tv_id);
+        TextView tv_UID = v.findViewById(R.id.tv_UID);
         TextView tv_nickName = v.findViewById(R.id.tv_nickName);
+        TextView tv_id = v.findViewById(R.id.tv_id);
         if (bgcolor == Color.LTGRAY) {
             v.setBackgroundColor(Color.WHITE);
-            hashSet.remove(String.valueOf(tv_id.getText()));
+            hashSet.remove(String.valueOf(tv_UID.getText()));
             hashSet2.remove(String.valueOf(tv_nickName.getText()));
+            hashSet3.remove(String.valueOf(tv_id.getText()));
         } else {
             v.setBackgroundColor(Color.LTGRAY);
-            hashSet.add(String.valueOf(tv_id.getText()));
+            hashSet.add(String.valueOf(tv_UID.getText()));
             hashSet2.add(String.valueOf(tv_nickName.getText()));
+            hashSet3.add(String.valueOf(tv_id.getText()));
         }
     }
 
     public void btv_Invite_Friend_Clicked(View v) {
         ArrayList<String> friend_arr = new ArrayList<>(hashSet);
         ArrayList<String> friend_arr2 = new ArrayList<>(hashSet2);
-        if(friend_arr.size() == 0){
+        if (friend_arr.size() == 0) {
             Toast.makeText(getApplicationContext(), "1명 이상 선택", Toast.LENGTH_SHORT).show();
             return;
         }
@@ -160,6 +123,47 @@ public class Invite_Friend extends AppCompatActivity {
         resultIntent.putExtra("friends2", friend_arr2);
         setResult(RESULT_OK, resultIntent);
         finish();
+    }
+
+    public void view_my_all_friend() {
+        recyclerView = findViewById(R.id.inviteFriendsRecyclerview); // 아이디 연결
+        recyclerView.setHasFixedSize(true);//리사이클러뷰 성능 강화
+        layoutManager = new LinearLayoutManager(this);//콘텍스트 자동입력
+        recyclerView.setLayoutManager(layoutManager);
+        ArrayList<User> userArrayList = new ArrayList<>();// User 객체를 담을 ArrayList(Adapter쪽으로 날릴 것임)
+        database = FirebaseDatabase.getInstance();//파이어베이스 데이터베이스 연결
+        getInviteFriendListValueEventLister2 = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                userArrayList.add(snapshot.getValue(User.class));
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        };
+        getInviteFriendListValueEventLister = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                userArrayList.clear(); //기존 배열리스트를 초기화
+                User me = snapshot.getValue(User.class);
+                String[] friends = me.getFriendsUID().split(" ");//위에서 필터링한 객체의 FriendsId를 공백을 기준으로 스플릿 해서 배열에 저장
+                for (String friend : friends) {
+                    databaseReference2 = database.getReference("User").child(friend);
+                    databaseReference2.addListenerForSingleValueEvent(getInviteFriendListValueEventLister2);
+                    databaseReference2.removeEventListener(getInviteFriendListValueEventLister2);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        };
+        databaseReference = database.getReference("User").child(UID);//DB테이블 연결, 파이어베이스 콘솔에서 User에 접근
+        databaseReference.addValueEventListener(getInviteFriendListValueEventLister);
+        adapter = new User_List_Adapter(userArrayList, this);
+        recyclerView.setAdapter(adapter); //리사이클러뷰에 어댑터 연결
     }
 }
 
