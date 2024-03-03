bb0b35fcd254b7ecf963c13cdb9003fd7ea204f5
cho-stone <cba64511@gmail.com>
2024-03-03 23:10:15 +0900

    add help home

--- app/src/main/java/com/PACOsoft/promise_betting/view/Home_Help.java
+++ app/src/main/java/com/PACOsoft/promise_betting/view/Home_Help.java
@@ -2,6 +2,7 @@ package com.PACOsoft.promise_betting.view;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,5 +14,9 @@ public class Home_Help extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_help);
+    }
+
+    public void home_help_close(View view){
+        finish();
     }
 }
